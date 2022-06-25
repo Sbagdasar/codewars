a6f54f7e5768c27734fc67685f51668e093a99dc
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-25 17:26:43 +0300

    task2

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-var replaceDots = function(str) {
-    return str.replace(/[.]/g, '-');
+// var replaceDots = function(str) {
+//     return str.replace(/[.]/g, '-');
+//   }
+//  console.log(replaceDots("one.two.three")) 
+
+//task 2 2
+function powersOfTwo(n){
+    let arr=[];
+    for(let i=0; i<=n; i++){
+        arr.push(2**i)
+    }
+    return arr
   }
- console.log(replaceDots("one.two.three")) 
+  console.log(powersOfTwo(0))
+  console.log(powersOfTwo(1))
+  console.log(powersOfTwo(2))
