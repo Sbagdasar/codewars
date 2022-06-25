502e07ac9675cec2e11a5a9efb72fbcd05326390
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-25 17:54:09 +0300

    task4

--- script.js
+++ script.js
@@ -19,3 +19,24 @@
 //   function buildString(...template){
 //     return `I like ${template.join(', ')}!`;
 //   }
+var min = function(list){
+    let min = list[0];
+    for (let i = 0; i < list.length; i++) {
+        
+        if(list[i]<=min){
+            min = list[i]
+        }
+    }
+    return min;
+}
+
+var max = function(list){
+    let max = list[0];
+    for (let i = 0; i < list.length; i++) {
+        if(list[i]>=max){
+            max = list[i]
+        }
+    }
+    return max;
+}
+console.log(min([-52, 56, 30, 29, -54, 0, -110]))
