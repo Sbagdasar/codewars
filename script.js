18c9e90ce8d7fe95e32ef2fa0bc5333cc5ac4d85
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-25 17:55:46 +0300

    best practice added

--- script.js
+++ script.js
@@ -39,4 +39,7 @@ var max = function(list){
     }
     return max;
 }
+// best practice
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
 console.log(min([-52, 56, 30, 29, -54, 0, -110]))
