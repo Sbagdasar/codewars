8eb2ccbb9572e11d39e882106986907236cedcc3
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-25 18:18:42 +0300

    task5

--- script.js
+++ script.js
@@ -19,27 +19,52 @@
 //   function buildString(...template){
 //     return `I like ${template.join(', ')}!`;
 //   }
-var min = function(list){
-    let min = list[0];
-    for (let i = 0; i < list.length; i++) {
+// var min = function(list){
+//     let min = list[0];
+//     for (let i = 0; i < list.length; i++) {
         
-        if(list[i]<=min){
-            min = list[i]
-        }
-    }
-    return min;
-}
+//         if(list[i]<=min){
+//             min = list[i]
+//         }
+//     }
+//     return min;
+// }
 
-var max = function(list){
-    let max = list[0];
-    for (let i = 0; i < list.length; i++) {
-        if(list[i]>=max){
-            max = list[i]
-        }
-    }
-    return max;
-}
+// var max = function(list){
+//     let max = list[0];
+//     for (let i = 0; i < list.length; i++) {
+//         if(list[i]>=max){
+//             max = list[i]
+//         }
+//     }
+//     return max;
+// }
 // best practice
-const min = (list) => Math.min(...list);
-const max = (list) => Math.max(...list);
-console.log(min([-52, 56, 30, 29, -54, 0, -110]))
+// const min = (list) => Math.min(...list);
+// const max = (list) => Math.max(...list);
+// console.log(min([-52, 56, 30, 29, -54, 0, -110]))
+
+//task 5
+/*var comparator = function(a,b) {
+    // Your code here
+    return 0
+  }
+  best pr
+  song = [
+"On the 12th day of Christmas my true love gave to me",
+"12 drummers drumming,",
+"11 pipers piping,",
+"10 lords a leaping,", 
+"9 ladies dancing,",
+"8 maids a milking,",
+"7 swans a swimming,", 
+"6 geese a laying,", 
+"5 golden rings,", 
+"4 calling birds,",
+"3 French hens,", 
+"2 turtle doves and", 
+"a partridge in a pear tree."
+]
+
+var comparator = (a, b) => song.indexOf(a) - song.indexOf(b)
+*/
