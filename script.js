9750dcff308f8ae5226403a921f52d42748a1200
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-06-30 00:05:47 +0300

    added task 8 best practice

--- script.js
+++ script.js
@@ -129,3 +129,27 @@ const rps = (p1, p2) => {
         return "Draw!"
     }
 };
+// best practice
+const rps = (p1, p2) => {
+    if (p1 === p2) return "Draw!";
+    var rules = {rock: "scissors", paper: "rock", scissors: "paper"};
+    if (p2 === rules[p1]) {
+        return "Player 1 won!";
+    }
+    else {
+        return "Player 2 won!";
+    }
+};
+const rps = (p1, p2) => {
+    var map = {
+        'rock': 'scissors',
+        'scissors': 'paper',
+        'paper': 'rock'
+    };
+
+    if (p1 == p2) {
+        return 'Draw!';
+    } else {
+        return 'Player ' + (map[p1] == p2 ? 1 : 2) + ' won!';
+    }
+};
